b4e74a4820f6082363dc76be8884eec8af81860c
Shubin Pavel <shubin-work@mail.ru>
2023-09-08 00:56:13 +0300

    1v of player (над катологом, без модалки)

--- js/app.js
+++ js/app.js
@@ -1,43 +1,45 @@
 const API_KEY = "8c8e1a50-6322-4135-8875-5d40a5420d86";
 const API_URL_POPULAR =
-  "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1";
+    "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1";
 const API_URL_SEARCH =
-  "https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=";
+    "https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=";
 const API_URL_MOVIE_DETAILS = "https://kinopoiskapiunofficial.tech/api/v2.2/films/"
 
 getMovies(API_URL_POPULAR);
 
+// Подключение к API и загрузка .json ответа в константу respData
 async function getMovies(url) {
-  const resp = await fetch(url, {
-    headers: {
-      "Content-Type": "application/json",
-      "X-API-KEY": API_KEY,
-    },
-  });
-  const respData = await resp.json();
-  showMovies(respData);
+    const resp = await fetch(url, {
+        headers: {
+            "Content-Type": "application/json",
+            "X-API-KEY": API_KEY,
+        },
+    });
+    const respData = await resp.json();
+    showMovies(respData);
+    // console.log(respData)
 }
 
 function getClassByRate(vote) {
-  if (vote >= 7) {
-    return "green";
-  } else if (vote > 5) {
-    return "orange";
-  } else {
-    return "red";
-  }
+    if (vote >= 7) {
+        return "green";
+    } else if (vote > 5) {
+        return "orange";
+    } else {
+        return "red";
+    }
 }
-
+// Генерация каталога фильмов при загрузке/перезагрузке сайта
 function showMovies(data) {
-  const moviesEl = document.querySelector(".movies");
+    const moviesEl = document.querySelector(".movies");
 
-  // Очищаем предыдущие фильмы
-  document.querySelector(".movies").innerHTML = "";
+    // Очищаем предыдущие фильмы
+    document.querySelector(".movies").innerHTML = "";
 
-  data.films.forEach((movie) => {
-    const movieEl = document.createElement("div");
-    movieEl.classList.add("movie");
-    movieEl.innerHTML = `
+    data.films.forEach((movie) => {
+                const movieEl = document.createElement("div");
+                movieEl.classList.add("movie");
+                movieEl.innerHTML = `
         <div class="movie__cover-inner">
         <img
           src="${movie.posterUrlPreview}"
@@ -57,6 +59,7 @@ function showMovies(data) {
         <div class="movie__average movie__average--${getClassByRate(
           movie.rating
         )}">${movie.rating}</div>
+        <div class="movie__year">${movie.year}</div>
         `
         }
       </div>
@@ -66,6 +69,7 @@ function showMovies(data) {
   });
 }
 
+// Работа формы поиска
 const form = document.querySelector("form");
 const search = document.querySelector(".header__search");
 
@@ -76,12 +80,13 @@ form.addEventListener("submit", (e) => {
   if (search.value) {
     getMovies(apiSearchUrl);
 
-    search.value = "";
+    // search.value = "";
   }
 });
 
 // Modal
 const modalEl = document.querySelector(".modal");
+const playerEl = document.querySelector(".kinobox_player")
 
 async function openModal(id) {
   const resp = await fetch(API_URL_MOVIE_DETAILS + id, {
@@ -92,26 +97,74 @@ async function openModal(id) {
   });
   const respData = await resp.json();
   
-  modalEl.classList.add("modal--show");
-  document.body.classList.add("stop-scrolling");
+  playerEl.classList.remove("hidden")
+  // modalEl.classList.add("modal--show");
+  // document.body.classList.add("stop-scrolling");
 
-  modalEl.innerHTML = `
-    <div class="modal__card">
-      <img class="modal__movie-backdrop" src="${respData.posterUrl}" alt="">
-      <h2>
-        <span class="modal__movie-title">${respData.nameRu}</span>
-        <span class="modal__movie-release-year"> - ${respData.year}</span>
-      </h2>
-      <ul class="modal__movie-info">
-        <div class="loader"></div>
-        <li class="modal__movie-genre">Жанр - ${respData.genres.map((el) => `<span>${el.genre}</span>`)}</li>
-        ${respData.filmLength ? `<li class="modal__movie-runtime">Время - ${respData.filmLength} минут</li>` : ''}
-        <li >Сайт: <a class="modal__movie-site" href="${respData.webUrl}">${respData.webUrl}</a></li>
-        <li class="modal__movie-overview">Описание - ${respData.description}</li>
-      </ul>
-      <button type="button" class="modal__button-close">Закрыть</button>
-    </div>
-  `
+  new Kinobox('.kinobox_player', {
+    search: {
+        kinopoisk: `${id}`,
+        // imdb: 'tt0816692',
+        // title: 'Интерстеллар'
+    },
+    menu: {
+        default: 'menuList',
+        mobile: 'menuButton',
+        format: '{N} :: {T} ({Q})',
+        limit: 5,
+        open: false,
+    },
+    players: {
+        alloha: {
+            enable: true,
+            position: 1,
+            token: '{token}'
+        },
+        kodik: {
+            enable: false,
+            position: 2,
+            token: '{token}'
+        },
+        // ...
+    },
+    params: {
+        all: {
+            poster: 'https://example.org/poster.jpg',
+        },
+        alloha: {
+            autoplay: 1
+        },
+        kodik: {
+            hide_selectors: true
+        },
+        // ...
+    },
+    hide: ['videocdn', 'collaps'],
+    order: ['kodik', 'alloha'],
+  }).init();
+
+  
+  // modalEl.innerHTML = `
+  //   <div class="modal__card">
+  //     <img class="modal__movie-backdrop" src="${respData.posterUrl}" alt="">
+  //     <h2>
+  //       <span class="modal__movie-title">${respData.nameRu}</span>
+  //       <span class="modal__movie-release-year"> - ${respData.year}</span>
+  //     </h2>
+  //     <ul class="modal__movie-info">
+  //       <div class="loader"></div>
+  //       <li class="modal__movie-genre">Жанр - ${respData.genres.map((el) => `<span>${el.genre}</span>`)}</li>
+  //       ${respData.filmLength ? `<li class="modal__movie-runtime">Время - ${respData.filmLength} минут</li>` : ''}
+  //       <li >Сайт: <a class="modal__movie-site" href="${respData.webUrl}">${respData.webUrl}</a></li>
+  //       <li class="modal__movie-overview">Описание - ${respData.description}</li>
+  //     </ul>
+  //     <button type="button" class="modal__button-close">Закрыть</button>
+  //   </div>
+  // `
+
+  window.scrollTo({top: 0, behavior: 'smooth'});
+
+
   const btnClose = document.querySelector(".modal__button-close");
   btnClose.addEventListener("click", () => closeModal());
 }
