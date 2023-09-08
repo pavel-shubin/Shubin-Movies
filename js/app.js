e3682d6ad3c448c29bad6b2cd0636e6e13422efd
Shubin Pavel <shubin-work@mail.ru>
2023-09-08 18:46:25 +0300

    Добавлены 2 кнопки: Топ 2023 и Лучшие фильмы

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
 const API_KEY = "8c8e1a50-6322-4135-8875-5d40a5420d86";
 const API_URL_POPULAR =
     "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1";
+const API_URL_BEST =
+    "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_250_BEST_FILMS&page=1";
 const API_URL_SEARCH =
     "https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=";
-const API_URL_MOVIE_DETAILS = "https://kinopoiskapiunofficial.tech/api/v2.2/films/"
+const API_URL_MOVIE_DETAILS =
+    "https://kinopoiskapiunofficial.tech/api/v2.2/films/"
+const API_URL_POPULAR_CATALOG =
+    "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=";
+const API_URL_BEST_CATALOG =
+    "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_250_BEST_FILMS&page=";
 
 getMovies(API_URL_POPULAR);
 
@@ -144,23 +151,23 @@ async function openModal(id) {
   }).init();
 
   
-  // modalEl.innerHTML = `
-  //   <div class="modal__card">
-  //     <img class="modal__movie-backdrop" src="${respData.posterUrl}" alt="">
-  //     <h2>
-  //       <span class="modal__movie-title">${respData.nameRu}</span>
-  //       <span class="modal__movie-release-year"> - ${respData.year}</span>
-  //     </h2>
-  //     <ul class="modal__movie-info">
-  //       <div class="loader"></div>
-  //       <li class="modal__movie-genre">Жанр - ${respData.genres.map((el) => `<span>${el.genre}</span>`)}</li>
-  //       ${respData.filmLength ? `<li class="modal__movie-runtime">Время - ${respData.filmLength} минут</li>` : ''}
-  //       <li >Сайт: <a class="modal__movie-site" href="${respData.webUrl}">${respData.webUrl}</a></li>
-  //       <li class="modal__movie-overview">Описание - ${respData.description}</li>
-  //     </ul>
-  //     <button type="button" class="modal__button-close">Закрыть</button>
-  //   </div>
-  // `
+  modalEl.innerHTML = `
+    <div class="modal__card">
+      <img class="modal__movie-backdrop" src="${respData.posterUrl}" alt="">
+      <h2>
+        <span class="modal__movie-title">${respData.nameRu}</span>
+        <span class="modal__movie-release-year"> - ${respData.year}</span>
+      </h2>
+      <ul class="modal__movie-info">
+        <div class="loader"></div>
+        <li class="modal__movie-genre">Жанр - ${respData.genres.map((el) => `<span>${el.genre}</span>`)}</li>
+        ${respData.filmLength ? `<li class="modal__movie-runtime">Время - ${respData.filmLength} минут</li>` : ''}
+        <li >Сайт: <a class="modal__movie-site" href="${respData.webUrl}">${respData.webUrl}</a></li>
+        <li class="modal__movie-overview">Описание - ${respData.description}</li>
+      </ul>
+      <button type="button" class="modal__button-close">Закрыть</button>
+    </div>
+  `
 
   window.scrollTo({top: 0, behavior: 'smooth'});
 
@@ -185,3 +192,28 @@ window.addEventListener("keydown", (e) => {
     closeModal();
   }
 })
+
+
+// Работа кнопок каталогов
+const btnActual = document.querySelector(".catalog-actual__top");
+const btnBest = document.querySelector(".catalog-alltime__top");
+
+window.addEventListener("click", (e) => {
+  if (e.target === btnActual) {
+    // Очищаем предыдущие фильмы
+    document.querySelector(".movies").innerHTML = "";
+
+    // Подгружаем карточки с фильмами
+    getMovies(API_URL_POPULAR);
+  }
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target === btnBest) {
+    // Очищаем предыдущие фильмы
+    document.querySelector(".movies").innerHTML = "";
+
+    // Подгружаем карточки с фильмами
+    getMovies(API_URL_BEST);
+  }
+});
